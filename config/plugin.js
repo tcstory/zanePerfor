ce9203a78b267a321d114f2264aa4b6e35c11cf2
再见田园犬 <tcstory@163.com>
2020-09-02 17:39:08 +0800

    feat: 移除掉 egg-router-plus

--- config/plugin.js
+++ config/plugin.js
@@ -16,11 +16,6 @@ exports.mongoose = {
     package: 'egg-mongoose',
 };
 
-exports.routerPlus = {
-    enable: true,
-    package: 'egg-router-plus',
-};
-
 exports.cors = {
     enable: true,
     package: 'egg-cors',
